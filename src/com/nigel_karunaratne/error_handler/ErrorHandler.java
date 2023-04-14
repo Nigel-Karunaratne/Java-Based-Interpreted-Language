a0c1c3c8f496759efff216527326b69e91287d54
Nigel-Karunaratne <nigel.karunaratne36@gmail.com>
2023-04-14 10:38:07 -0400

    added throwRuntimeError + made methods camelCase

--- src/com/nigel_karunaratne/error_handler/ErrorHandler.java
+++ src/com/nigel_karunaratne/error_handler/ErrorHandler.java
@@ -1,28 +1,35 @@
 package com.nigel_karunaratne.error_handler;
 
+import com.nigel_karunaratne.tokens.Token;
+
 public final class ErrorHandler {
-    public static void OutputWarning(String message) {
+    public static void outputWarning(String message) {
         System.out.printf("WARNING | ????, ???? | %s\n", message);
     }
 
-    public static void OutputWarning(String message, int line, int col) {
+    public static void outputWarning(String message, int line, int col) {
         System.out.printf("WARNING | %4d, %4d | %s\n", line, col, message);
     }
 
-    public static void OutputException(String message) {
+    public static void outputException(String message) {
         System.out.printf("ERROR   | ????, ???? | %s\n", message);
     }
 
-    public static void OutputException(String message, int line, int col) {
+    public static void outputException(String message, int line, int col) {
         System.out.printf("ERROR   | %4d, %4d | %s\n", line, col, message);
     }
 
 
-    public static void OutputFatalException() {
+    public static void outputFatalException() {
 
     }
 
-    public static void DebugOutput(String message) {
+    public static void debugOutput(String message) {
         System.out.printf("DEBUG   | ????, ???? | %s\n", message);
+    }
+
+    public static RuntimeError throwRuntimeError(Token currentToken, String errorMessage) {
+        ErrorHandler.outputException(errorMessage, currentToken.line, currentToken.column);
+        return new RuntimeError();
     }
 }
